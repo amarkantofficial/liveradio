2c890d7f2773ba59c76a4c15cb4c88df563407a9
Amar kant <amarkoiri2@gmail.com>
2021-09-20 18:40:13 +0530

    Add files via upload

---
+++ hide-banner.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>302 Found</title>
+</head><body>
+<h1>Found</h1>
+<p>The document has moved <a href="http://103.217.221.64/snehtv/404.php">here</a>.</p>
+<hr>
+<address>Apache/2.4.38 (Debian) Server at 103.217.221.64 Port 80</address>
+</body></html>
